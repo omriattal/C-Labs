17fdaa3ee4c72460e2b664180ed7c716dd6f2be3
caspl202 <omriattal>
2020-04-23 11:29:28 +0300

    fully implemented

--- lab4/task1/encoder.c
+++ lab4/task1/encoder.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[], char *envp[])
     char* return_from_sys_call;
     char input[1] = "\0";
     int i;
-    for (i = 1; argv[i] != nullchar && i <= 3; i++) {
+    for (i = 1; argv[i] != nullchar && i <= argc; i++) {
         if (strcmp(argv[i], "-D") == 0) {
             debugMode = true;;
         }
@@ -41,21 +41,35 @@ int main(int argc, char *argv[], char *envp[])
             output_file_descriptor = system_call(SYS_OPEN,argv[i]+ OFFSET,WRONLY);    
             if(output_file_descriptor < 0) {
                 system_call(SYS_WRITE,STDOUT,"Invalid output file!\n",21);
-                system_call(SYS_EXIT,UNSUCSSEFUL_EXIT_CODE,0,0);
+                system_call(SYS_EXIT,UNSUCSSEFUL_EXIT_CODE);
             }
         }
         if(strncmp(argv[i],"-i",2) == 0) {
             input_file_descriptor = system_call(SYS_OPEN,argv[i]+OFFSET,O_RDONLY);
             if(input_file_descriptor < 0) {
                 system_call(SYS_WRITE,STDOUT,"Invalid input file!\n",20);
-                system_call(SYS_EXIT,UNSUCSSEFUL_EXIT_CODE,0,0);
+                system_call(SYS_EXIT,UNSUCSSEFUL_EXIT_CODE);
             }
         }
     }
 
     
-    while (system_call(SYS_READ, input_file_descriptor, input, 1) >= 1)
+    while (true)
     {
+        int returned_from_read = system_call(SYS_READ, input_file_descriptor, input, 1);
+        if(returned_from_read<=0) {
+            break;
+        }
+        if(input[0] != newline_ASCII_NUMBER) {
+        return_from_sys_call= itoa(returned_from_read);
+        id_sys_call = itoa(SYS_READ);
+        if(debugMode) {
+            system_call(SYS_WRITE,STDERR,"",1);
+            system_call(SYS_WRITE,STDERR,return_from_sys_call,strlen(return_from_sys_call));;
+            system_call(SYS_WRITE,STDERR,"\t",1);
+            system_call(SYS_WRITE,STDERR,id_sys_call,strlen(id_sys_call));
+            system_call(SYS_WRITE,STDERR,"\n",1);
+        }
         if (isLowerCaseLetter(input[0]))
         {
             input[0] = input[0] - TO_UPPER_CASE;
@@ -63,11 +77,18 @@ int main(int argc, char *argv[], char *envp[])
          return_from_sys_call = itoa(system_call(SYS_WRITE, output_file_descriptor, input, 1));
          id_sys_call = itoa(SYS_WRITE);
         if(debugMode) {
+            system_call(SYS_WRITE,STDERR,"",1);
             system_call(SYS_WRITE,STDERR,return_from_sys_call,strlen(return_from_sys_call));;
             system_call(SYS_WRITE,STDERR,"\t",1);
             system_call(SYS_WRITE,STDERR,id_sys_call,strlen(id_sys_call));
             system_call(SYS_WRITE,STDERR,"\n",1);
         }
+        
+    } else
+    {
+        system_call(SYS_WRITE,output_file_descriptor,"\n",1);
+    }
+    
     }
     system_call(SYS_CLOSE,input_file_descriptor);
     system_call(SYS_CLOSE,output_file_descriptor);
